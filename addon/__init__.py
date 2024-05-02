50c55ceda43c099f8b31873dedaaa6219fb449a4
Your Name <you@example.com>
2024-05-02 20:51:26 +0300

    fix again

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,17 +1,4 @@
 
-bl_info = {
-    "name": "AutoMDL",
-    "author": "NvC_DmN_CH",
-    "version": (1, 0),
-    "blender": (2, 8, 0),
-    "location": "View3D > Sidebar > AutoMDL",
-    "description": "Compiles models for Source where the blend project file is",
-    "warning": "",
-    "wiki_url": "",
-    "category": "3D View"
-}
-
-
 import bpy
 import os
 import subprocess
